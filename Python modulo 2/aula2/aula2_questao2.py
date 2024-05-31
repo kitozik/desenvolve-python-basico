84353062de4ca556388a9801bebc6dc995d9f4d4
Nicolas Moreira <166874872+kitozik@users.noreply.github.com>
2024-05-31 11:12:32 -0300

    upload aula 2

---
+++ Python modulo 2/aula2/aula2_questao2.py
@@ -0,0 +1,14 @@
+## Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+##Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+##Some os valores da segunda e terceira variável e armazene em outra variável.
+##Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+
+resultado = "o resultado é:"
+valor1 = 10
+valor2 = 3.5
+
+resposta = valor1 + valor2
+print(resultado, "(string)")
+print(valor1, "(int)" )
+print(valor2, "(float)")
+print(resposta, "(float)")
